590b3b8f76e5f7f5117b04770d66570ae72f1c4f
Turqueti <turquetivitor@gmail.com>
2020-05-14 06:32:22 -0300

    limpando o repositorio

--- contaCasos.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-pathToTests=$PWD/casos_de_teste
-# echo $pathToTests
-var=$(ls $pathToTests -l| grep .in|wc -l)
-
-echo $var
--- criaCasos.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in {1..5}
-do
-    echo $i > $PWD/casos_de_teste/$i.in
-    echo $(( $i + 1 )) > $PWD/casos_de_teste/$i.out
-done
-
